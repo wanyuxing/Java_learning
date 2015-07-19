a8ad39f59c8021250a933d8da2c1a85bab99b17d
wanyuxing <wanyuxing@hotmail.com>
2015-07-19 22:41:29 +0800

    Henry Wan

--- Exercises3/src/Exercises3/ComboLock.java
+++ Exercises3/src/Exercises3/ComboLock.java
@@ -1,45 +1,40 @@
 package Exercises3;
 
+import java.util.Arrays;
+
 public class ComboLock {
-	private int tickPos;
-	private int secret1;
-	private int secret2;
-	private int secret3;
+	private int[] secret = new int[3];
+	private int[] ans = new int[3];
 	private int count;
-	private boolean secret1Open;
-	private boolean secret2Open;
-	private boolean secret3Open;
-	
+
 	public ComboLock(int secret1, int secret2, int secret3)
 	{
-		this.secret1 = secret1;
-		this.secret2 = secret2;
-		this.secret3 = secret3;
-		this.tickPos = 0;
+		this.secret[0] = secret1;
+		this.secret[1] = secret2 - 40;
+		this.secret[2] = secret3;
 		this.count = 0;
 	}
 	
 	public void reset()
 	{
-		this.tickPos = 0;
+		Arrays.fill(this.ans, 0);
 		this.count = 0;
 	}
 	
 	public void turnLeft(int ticks) 
 	{ 
-		this.tickPos -= ticks;
-		count++;
-		if (count == 2) { if (this.tickPos == (this.secret2 - 40)) { this.secret2Open = true;}}
+		if (this.count == 0) { this.ans[this.count] = -ticks;}
+		else { this.ans[this.count] = this.ans[this.count - 1] - ticks;}
+		this.count++;
 	}
 	
 	public void turnRight(int ticks) 
 	{ 
-		this.tickPos += ticks;
+		if (this.count == 0) { this.ans[this.count] = ticks;}
+		else { this.ans[this.count] = this.ans[this.count - 1] + ticks;}
 		this.count++;
-		if (count == 1) { if (this.tickPos == this.secret1) { this.secret1Open = true;}}
-		else if (count == 3) { if (this.tickPos == this.secret3) { this.secret3Open = true;}}
 	}
 	
 	public boolean open()
-	{ return this.secret1Open & this.secret2Open & this.secret3Open;}
+	{ return Arrays.equals(this.secret, this.ans);}
 }
--- Exercises3/src/Exercises3/Testing.java
+++ Exercises3/src/Exercises3/Testing.java
@@ -4,10 +4,14 @@ public class Testing {
 
 	public static void main(String[] args) 
 	{
-		Letter l = new Letter("Henry", "Yecheng");
-		l.addLine("I miss you");
-		l.addLine("I love you");
-		l.main();
+		ComboLock sd = new ComboLock(20, 30, 10);
+		sd.turnRight(20);
+		sd.turnLeft(30);
+		sd.reset();
+		sd.turnRight(20);
+		sd.turnLeft(30);
+		sd.turnRight(20);
+		System.out.println(sd.open());
 	}
 
 }
